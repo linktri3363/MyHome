ecb3f4ce1cc1ff688bd953dce0be147e9362e81f
Linktri <80237916+linktri3363@users.noreply.github.com>
2025-07-12 14:48:27 -0400

    Update and rename MyHome.lua to MyHome2.lua
    Here are the git commit changes for renaming the addon:
    
    
    Git Commit Changes
    Code 
    The git changes involve:
    
    Renaming the main file: MyHome.lua → MyHome2.lua
    Code changes in MyHome2.lua:
    Line 2
    
    
    
    
    # Rename the main file
    git mv MyHome.lua MyHome2.lua
    
    # Stage all changes
    git add .
    
    # Commit with descriptive message
    git commit -m "Rename addon from MyHome to MyHome2
    
    - Changed _addon.name from 'MyHome' to 'MyHome2'
    - Updated copyright notice to reference MyHome2
    - Kept original commands unchanged (myhome, mh, warp)
    - Updated README.md title to reflect new addon name
    - Renamed main file from MyHome.lua to MyHome2.lua"

--- MyHome2.lua
+++ MyHome2.lua
@@ -10,7 +10,7 @@ modification, are permitted provided that the following conditions are met:
     * Redistributions in binary form must reproduce the above copyright
     notice, this list of conditions and the following disclaimer in the
     documentation and/or other materials provided with the distribution.
-    * Neither the name of MyHome nor the
+    * Neither the name of MyHome2 nor the
     names of its contributors may be used to endorse or promote products
     derived from this software without specific prior written permission.
 
@@ -25,7 +25,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.]]
 
-_addon.name = 'MyHome'
+_addon.name = 'MyHome2'
 _addon.author = 'from20020516, Linktri'
 _addon.version = '1.1.1'
 _addon.commands = {'myhome','mh','warp'}
